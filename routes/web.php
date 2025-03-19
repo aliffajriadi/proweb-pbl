5aae8b69cff51c3a66862ac1a203e8dcbc75d395
kutu-guling <dwikyputradachi21@gmail.com>
2025-03-19 21:26:37 +0700

    Menambahkan Dashboard Admin

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\AdminDashboardController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('contact', [HomeController::class, 'contact']);
@@ -29,3 +30,5 @@ Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'listbarang'
 Route::get('/staff', [StaffController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('/admin/dashboard', [AdminDashboardController::class, 'show']);
